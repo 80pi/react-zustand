613fd8ad4e19515b121947ae1a86292ab217630a
80pi <gopivenkataajay5771@gmail.com>
2024-05-29 19:20:02 +0530

    feat: initial zustand setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import ApiCallContainer from "./pages/ApiCallContainer";
+import ButtonsContainer from "./pages/ButtonsContainer";
+
+function App() {
+  return (
+    <>
+      <ButtonsContainer />
+      <ApiCallContainer />
+    </>
+  );
+}
+
+export default App;
